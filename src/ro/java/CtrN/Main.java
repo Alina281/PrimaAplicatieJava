186729400438e5103999ef189eb22d6e720b8bdc
LENOVO <alina_pescariu@yahoo.com>
2021-09-11 14:39:48 +0300

    Am adaugat Javadoc

--- src/ro/java/CtrN/Main.java
+++ src/ro/java/CtrN/Main.java
@@ -1,17 +1,14 @@
 package ro.java.CtrN;
 
 public class Main {
-    //Aceasta este metoda main care porneste o aplicatie in Java
-    /*
-    Aici este un alt exemplu
-    de comantariu
-    pe mai multe linii
-     */
-    public static void main(/*arg este parametrul metodei main-comm in cod*/String[] args) {
-        /* Inainte de comentariu  nu se poate pune text
-         Aici vrem sa afisam pe consola intellij un mesaj ales de noi
-          */
 
+    /**
+     * Putem sa transmitem argumente aditionale  prin parametrul <b>args</b>
+     * Metoda main este metoda apelata de masina virtuala Java si care porneste o aplicatie Java
+     * @param args Acesta este parametrul cu care transmitem argumente programului nostru
+     * @author Pescariu Alina
+     */
+    public static void main(String[] args) {
         System.out.println("Invat limbajul Java si despre masina virtuala Java");
       
     }
