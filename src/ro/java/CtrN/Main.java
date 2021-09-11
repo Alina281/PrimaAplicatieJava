77520b1c326a4cfcac361311d6a58d6964698f43
LENOVO <alina_pescariu@yahoo.com>
2021-09-11 12:28:47 +0300

    modificare main.java - stergere comentariu

--- src/ro/java/CtrN/Main.java
+++ src/ro/java/CtrN/Main.java
@@ -1,9 +1,8 @@
 package ro.java.CtrN;
 
 public class Main {
-
-    //JAVA_HOME
     public static void main(String[] args) {
         System.out.println("Invat limbajul Java si despre masina virtuala Java");
+      
     }
 }
